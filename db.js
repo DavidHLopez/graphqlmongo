80852dde7bc38e5ae897f071744590b5ff5b99f4
David H Lopez <65629759+DavidHLopez@users.noreply.github.com>
2023-02-04 22:00:42 +0100

    Update db.js

--- db.js
+++ db.js
@@ -2,7 +2,7 @@ const { connect } = require("mongoose");
 
 const connectDb = async () => {
   try {
-    await connect(process.env.MONGODB_URI || "mongodb+srv://iwvuser:iwvuser1@pruebas.nabbcu0.mongodb.net/?retryWrites=true&w=majority");
+    await connect(process.env.MONGODB_URI || "mongodb+srv://iwvuser:XXXXXX@pruebas.nabbcu0.mongodb.net/?retryWrites=true&w=majority");
     console.log("Mongodb connected");
   } catch (error) {
     console.error(error);
